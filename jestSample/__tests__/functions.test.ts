346ce0614ee08ad7857a90e2581ca15dd6dfd99d
yoshino akira <akirayo.badpingpong@gmail.com>
2024-06-10 20:39:35 +0900

    課題2-1

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,28 @@
-// todo: ここに単体テストを書いてみましょう！
+import { asyncSumOfArray, sumOfArray } from "../functions";
+
+describe('functions', () => {
+  describe("sumOfArray", () => {
+    test("should return sum of array", () => {
+      const numbers = [1, 2, 3, 4, 5];
+      const result = sumOfArray(numbers);
+      expect(result).toBe(15);
+    })
+    test('空配列のときはエラー', () => {
+      const numbers: number[] = []
+      expect(() => sumOfArray(numbers)).toThrowError()
+    })
+  })
+  
+  describe("asyncSumOfArray", () => {
+    test("should return sum of array", async () => {
+      const numbers = [1, 2, 3, 4, 5];
+      const result = await asyncSumOfArray(numbers);
+      expect(result).toBe(15);
+    })
+
+    test('空配列のときはエラー', async () => {
+      const numbers: number[] = []
+      await expect(async() => asyncSumOfArray(numbers)).rejects.toThrowError()
+    })
+  })
+})
